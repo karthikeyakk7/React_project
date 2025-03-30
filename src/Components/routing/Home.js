52366b76799343c8f3f2db81145e72fc6c19fea5
karthikeyakk7 <mulkalakarthikeya@gmail.com>
2025-03-31 00:25:21 +0530

    Routing and useNavigate

--- src/Components/routing/Home.js
+++ src/Components/routing/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const [auth, setAuth]=useState(false);
+  const navigate = useNavigate();
+  if(auth){
+    navigate('/dashboard');
+    }
   return (
     <div>
+      <center>
       <h1> these is home page</h1>
+      <button onClick={()=> setAuth(true)}>enter</button>
+      </center>
     </div>
   )
 }
